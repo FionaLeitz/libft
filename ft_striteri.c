f6386c0e19101c9ec96cbf255d763cb4f1de721f
Fiona LEITZ <fleitz@e1r6p8.42.fr>
2021-12-07 16:24:26 +0100

    protection

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: fleitz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 10:13:17 by fleitz            #+#    #+#             */
-/*   Updated: 2021/12/07 09:29:49 by fleitz           ###   ########.fr       */
+/*   Updated: 2021/12/07 16:23:35 by fleitz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i])
 	{
